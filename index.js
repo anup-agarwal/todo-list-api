84c9ca3551b9be0344a5254fdd3f742e4d41856d
Anup Agarwal <hello@anupagarwal.com>
2021-12-01 21:44:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 5000;
+const { v4: uuid } = require("uuid");
+
+app.use(require("body-parser").json());
+app.use(require("cors")());
+
+let items = [];
+
+app.get("/todos", (req, res) => {
+  console.log(items);
+  res.send(items);
+});
+
+app.delete("/todo/:id", (req, res) => {
+  let found = false;
+  items = items.filter((item) => {
+    if (item.id !== req.params.id) found = true;
+    return item.id !== req.params.id;
+  });
+  if (found) return res.send({ msg: `${req.params.id} deleted successfully` });
+  return res.status(404).send({ msg: `${req.params.id} not found` });
+});
+
+app.post("/createtodo", (req, res) => {
+  const item = req.body.item;
+  const id = uuid();
+  items.push({ item, id });
+  res.send({ item, id });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
