9300b5576ae86a571098e4f2a70f497b55400167
Anup Agarwal <hello@anupagarwal.com>
2021-12-02 03:53:56 +0530

    cosmetic changes

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const { v4: uuid } = require("uuid");
+const morgan = require("morgan");
 
 app.use(require("body-parser").json());
 app.use(require("cors")());
+app.use(morgan("tiny"));
 
 let items = [];
 
 app.get("/todos", (req, res) => {
-  console.log(items);
   res.send(items);
 });
 
@@ -20,7 +21,6 @@ app.delete("/todo/:id", (req, res) => {
       if (item.id === req.params.id) found = true;
       return item.id !== req.params.id;
     });
-    console.log(items);
     if (found)
       return res.send({ msg: `${req.params.id} deleted successfully` });
     return res.status(404).send({ msg: `${req.params.id} not found` });
@@ -33,7 +33,12 @@ app.post("/createtodo", (req, res) => {
   const item = req.body.item;
   const id = uuid();
   items.push({ item, id });
-  res.send({ item, id });
+  res.status(201).send({ item, id });
+});
+
+app.delete("/todos", (req, res) => {
+  items = [];
+  res.status(200).send({ items });
 });
 
 app.listen(process.env.PORT, () =>
